d6a97a2ccb67e8f595973c59737e7468df1912e8
Burak0z <94439714+Burak0z@users.noreply.github.com>
2021-12-06 13:36:51 +0300

    Create game.py

---
+++ game.py
@@ -0,0 +1,52 @@
+import random
+
+num=random.randint(1,100)
+
+print("WELCOME TO GUESS ME!")
+print("I'm thinking of a number between 1 and 100")
+print("If your guess is more than 10 away from my number, I'll tell you you're COLD")
+print("If your guess is within 10 of my number, I'll tell you you're WARM")
+print("If your guess is farther than your most recent guess, I'll say you're getting COLDER")
+print("If your guess is closer than your most recent guess, I'll say you're getting WARMER")
+print("LET'S PLAY!")
+
+guesses=[0]
+
+while True:
+    guess= int(input("I'm thinking of a number between 1 and 100. \n What is Your guess?"))
+    
+    if guess <1 or guess >100:
+        print("YOU ARE OUT OF BOUNDS!  PLEASE TRY AGAIN:")
+    continue
+
+    pass
+  
+  while True:
+ # we can copy the code from above to take an input
+
+    guess= int(input("I'm thinking of a number between 1 and 100. \n What is Your guess?"))
+    
+    if guess <1 or guess >100:
+        print("YOU ARE OUT OF BOUNDS!  PLEASE TRY AGAIN:")
+        continue
+
+    if guess==num:
+        print(f'CONGRATULATIONS YOU HAVE GUESSED THE CORRECT NUMBER ON YOUR {len(guesses)} rd TRIAL!')
+        break 
+
+    guesses.append(guess) 
+
+    if guesses[-2]:  
+        if abs(num-guess) < abs(num-guesses[-2]):
+            print('WARMER!')
+        else:
+            print('COLDER!')
+   
+    else:
+        if abs(num-guess) <= 10:
+            print('WARM!')
+        else:
+            print('COLD!')
+
+    pass
+  
